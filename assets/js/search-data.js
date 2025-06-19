4aecd3bfb737f8935ae5dd2c53782991f5dce4e9
niMgnoeSeeL <nim.gnoes.eel@gmail.com>
2025-06-19 02:12:07 +0000

    Deploying to gh-pages from @ niMgnoeSeeL/nimgnoeseel.github.io@8fd5ac7f22b0ea64d50da0abe235449d03f2e9de 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,12 +52,7 @@ ninja.data = [{
             window.location.href = "/blog/2024/blog-start/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-mortar-board-i-successfully-defended-my-ph-d-thesis",
+      },{id: "news-mortar-board-i-successfully-defended-my-ph-d-thesis",
           title: ':mortar_board: I successfully defended my Ph.D. thesis.',
           description: "",
           section: "News",},{id: "news-microphone-i-presented-an-invited-talk-at-handong-global-university-on-statistical-program-dependence-analysis",
@@ -117,52 +112,7 @@ ninja.data = [{
           section: "News",},{id: "news-mortar-board-i-have-been-invited-to-serve-as-a-program-committee-member-for-fse-2026",
           title: ':mortar_board: I have been invited to serve as a program committee member for...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
+          section: "News",},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
